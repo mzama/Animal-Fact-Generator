c51b837f0977f7d2f9cc904bb2ff978bfd78190b
Matteo Zamaria <matteozamaria@gmail.com>
2020-04-22 03:17:05 -0400

    Finish core functionality

---
+++ data.js
@@ -0,0 +1,25 @@
+var data = {
+    "Defaults":[
+        {
+            "type":"turtle",
+            "link":"https://en.wikipedia.org/wiki/Turtle"
+        }
+    ],
+    "Facts":[
+        {
+            "factHeading":"Turtle's breathe through their butts.",
+            "factBody":"Turtle's employ 'cloacal respiration', meaning they can perform gas exchange through their butt. This helps during hibernation.",
+            "type":"turtle",
+            "link:":"",
+            "image":""
+            
+        },
+        {
+            "factHeading":"Turtle's are their shell, not just in them.",
+            "factBody":"Turtle (and tortoise!) shells are a part of their skeletons, and are fused to their spines. Turtle's do not just live in their shell, their shell is an important part of their body",
+            "type":"turtle",
+            "link:":"https://twitter.com/AdaMcVean/status/1252689051724980227",
+            "image":"Turtle_Skeleton.jpg"            
+        }
+    ]
+};
--- main.js
+++ main.js
@@ -1,4 +1,39 @@
-let fs = require("fs");
+var lastIndex = 0;
+document.addEventListener("DOMContentLoaded", function() {
+    SetFact(lastIndex);
+    document.querySelector("#newFact").addEventListener("click", SetFact);
+});
 
-let data = fs.readFileSync("data.json", "utf-8");
-console.log(JSON.parse(data));
+function SetFact(_lI) {
+    let newFactIndex = Math.floor(Math.random() * (data["Facts"].length));
+    
+    while (newFactIndex === lastIndex) {
+        newFactIndex = Math.floor(Math.random() * (data["Facts"].length));
+    }
+    let newFact = data["Facts"][newFactIndex];
+    lastIndex = newFactIndex;
+
+    document.querySelector(".fact-title h2").innerHTML = newFact["factHeading"];
+    document.querySelector(".fact-copy p").innerHTML = newFact["factBody"];
+    if (newFact["image"]) {
+        document.querySelector("#factImg").setAttribute("src", `Images/Fact/${newFact["image"]}`);
+    } else {
+        document.querySelector("#factImg").setAttribute("src", "");
+    }
+
+    if (newFact["link"]) {
+        document.querySelector(".fact-link a").setAttribute("href", newFact["link"]);
+    } else {
+        document.querySelector(".fact-link a").setAttribute("href", data["Defaults"].filter(d => d.type === newFact["type"])[0].link);
+    }
+
+    switch (newFact["type"]) {        
+        case "turtle":
+            let newImgIndex = Math.ceil(Math.random() * 3);
+            document.querySelector(".bg-image").style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(Images/BG/Turtle/Turtle_${newImgIndex}.jpg)`;
+            break;
+        default:
+            document.querySelector(".bg-image").style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(Images/BG/Turtle/Turtle_${newImgIndex}.jpg)`;
+            break;
+    }
+}

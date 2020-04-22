d53b705f7408e5eaf95e6ae674fe8d8e8e078583
Matteo Zamaria <matteozamaria@gmail.com>
2020-04-22 16:09:39 -0400

    Finish base site: polish, images, mobile design.

---
+++ js/data.js
@@ -0,0 +1,50 @@
+var data = {
+    "Defaults":[
+        {
+            "type":"Turtle",
+            "link":"https://en.wikipedia.org/wiki/Turtle",
+            "bgCount":"5"
+        },
+        {
+            "type":"Koala",
+            "link":"https://en.wikipedia.org/wiki/Koala",
+            "bgCount":"5"
+        },
+        {
+            "type":"Ant",
+            "link":"https://en.wikipedia.org/wiki/Ant",
+            "bgCount":"5"
+        }
+    ],
+    "Facts":[
+        {
+            "factHeading":"Turtle's breathe through their butts.",
+            "factBody":"Turtles can perform simple gas exchange via 'cloacal respiration'. Oxygen and Carbon Dioxide diffues through their cloacas, allowing turtles to continue breathing during hibernation.",
+            "type":"Turtle",
+            "link":"https://www.mcgill.ca/oss/article/did-you-know/turtles-breathe-out-their-butt",
+            "image":""
+            
+        },
+        {
+            "factHeading":"Turtle's are their shell, they're not just living inside them.",
+            "factBody":"Turtle (and tortoise!) shells are a part of their skeletons, and are fused to their spines. Turtle's do not just live within their shell, their shell is an important part of their body.",
+            "type":"Turtle",
+            "link":"https://twitter.com/AdaMcVean/status/1252689051724980227",
+            "image":"Turtle_Skeleton.jpg"            
+        },
+        {
+            "factHeading":"Koala Bears have fingerprints just like humans!",
+            "factBody":"This seems to enhance their sense of touch, and allows them to be very picky eater's when feeling around eucalyptus trees for the best leaves.",
+            "type":"Koala",
+            "link":"https://www.mcgill.ca/oss/article/did-you-know/koalas-have-fingerprints-just-humans",
+            "image":""            
+        },
+        {
+            "factHeading":"Ants are farmers!",
+            "factBody":"Leafcutter ants build special structures to cultivate a special fungus they grow to eat. The leaves they cut are brought back to the colony for the fungus to grow on. Other species of ants have been observed herding and protecting aphids for a nectar they secrete.",
+            "type":"Ant",
+            "link":"https://www.mcgill.ca/oss/article/did-you-know/did-you-know-leafcutter-ants-are-farmers-who-grow-fungi",
+            "image":""            
+        }
+    ]
+};
--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 var lastIndex = 0;
 document.addEventListener("DOMContentLoaded", function() {
-    SetFact(lastIndex);
+    SetFact();
     document.querySelector("#newFact").addEventListener("click", SetFact);
 });
 
-function SetFact(_lI) {
+function SetFact() {
+    window.scrollTo(0,0);
+
     let newFactIndex = Math.floor(Math.random() * (data["Facts"].length));
     
     while (newFactIndex === lastIndex) {
@@ -27,13 +29,16 @@ function SetFact(_lI) {
         document.querySelector(".fact-link a").setAttribute("href", data["Defaults"].filter(d => d.type === newFact["type"])[0].link);
     }
 
-    switch (newFact["type"]) {        
-        case "turtle":
-            let newImgIndex = Math.ceil(Math.random() * 3);
-            document.querySelector(".bg-image").style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(Images/BG/Turtle/Turtle_${newImgIndex}.jpg)`;
-            break;
-        default:
-            document.querySelector(".bg-image").style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(Images/BG/Turtle/Turtle_${newImgIndex}.jpg)`;
-            break;
+    let bgURL = "";
+    let newImgIndex = 0;
+    let bgImgCount = data["Defaults"].filter(d => d.type === newFact["type"])[0]["bgCount"];
+
+    if (bgImgCount < 1) {
+        bgURL = "Images/BG/Default/Default_1.jpg";
+    } else {
+        newImgIndex = Math.ceil(Math.random() * bgImgCount)
+        bgURL=`Images/BG/${newFact["type"]}/${newFact["type"]}_${newImgIndex}.jpg`;
     }
+
+    document.querySelector(".bg-image").style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.35)), url(${bgURL})`;
 }

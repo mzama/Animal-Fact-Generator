2ba84d1c05a0ad035af26c549b67d3f8a8956224
Matteo Zamaria <matteozamaria@gmail.com>
2020-04-26 13:02:40 -0400

    Add many facts!

--- js/data.js
+++ js/data.js
@@ -8,7 +8,7 @@ var data = {
         {
             "type":"Marsupial",
             "link":"https://en.wikipedia.org/wiki/Marsupial",
-            "bgCount":"5"
+            "bgCount":"10"
         },
         {
             "type":"Ant",
@@ -18,12 +18,12 @@ var data = {
         {
             "type":"Cat",
             "link":"https://en.wikipedia.org/wiki/Cat",
-            "bgCount":"5"
+            "bgCount":"14"
         },
         {
             "type":"Dog",
             "link":"https://en.wikipedia.org/wiki/Dog",
-            "bgCount":"7"
+            "bgCount":"11"
         },
         {
             "type":"Bug",
@@ -33,7 +33,7 @@ var data = {
         {
             "type":"Fish",
             "link":"https://en.wikipedia.org/wiki/Fish",
-            "bgCount":"5"
+            "bgCount":"9"
         },
         {
             "type":"Rabbit",
@@ -51,11 +51,6 @@ var data = {
             "bgCount":"8"
         },
         {
-            "type":"Owl",
-            "link":"https://en.wikipedia.org/wiki/Owl",
-            "bgCount":"8"
-        },
-        {
             "type":"Rat",
             "link":"https://en.wikipedia.org/wiki/Rat",
             "bgCount":"6"
@@ -69,6 +64,26 @@ var data = {
             "type":"Pangolin",
             "link":"https://en.wikipedia.org/wiki/Pangolin",
             "bgCount":"2"
+        },
+        {
+            "type":"Bear",
+            "link":"https://en.wikipedia.org/wiki/Bear",
+            "bgCount":"6"
+        },
+        {
+            "type":"Monotreme",
+            "link":"https://en.wikipedia.org/wiki/Monotreme",
+            "bgCount":"5"
+        },
+        {
+            "type":"Bird",
+            "link":"https://en.wikipedia.org/wiki/Bird",
+            "bgCount":"17"
+        },
+        {
+            "type":"Arctic",
+            "link":"https://en.wikipedia.org/wiki/6",
+            "bgCount":"6"
         }
     ],
     "Facts":[
@@ -82,7 +97,7 @@ var data = {
         },
         {
             "factHeading":"You cannot remove a turtle's shell. They are their shell.",
-            "factBody":"Turtle (and tortoise) shells are a part of their skeletons, and are fused to their spines. Turtle's do not just live within their shell, their shell is an important part of their body.",
+            "factBody":"Turtle shells are a part of their skeletons, and are fused to their spines. Turtle's do not just live within their shell, their shell is an important part of their body.",
             "type":"Turtle",
             "link":"https://twitter.com/AdaMcVean/status/1252689051724980227",
             "image":"Turtle_Skeleton.jpg"            
@@ -102,7 +117,7 @@ var data = {
             "image":""            
         },
         {
-            "factHeading":"Bells attached to cat’s collars will not harm their hearing, but they are effective at preventing them hunting wildlife!",
+            "factHeading":"A bell attached to a cat’s collar will not harm their hearing, but they are effective at preventing them hunting wildlife.",
             "factBody":"Despite they're highly sensitive hearing, there is no evidence that a bell on a cat's collar causes harm to them or damage to their hearing. However, a bell on a collar does appear to reduce the amount of prey caught by an outdoor cat, although some fear the noise may make the cat more vulnerable to predators.",
             "type":"Cat",
             "link":"https://animalwellnessmagazine.com/bell-cats-collar/ ",
@@ -116,7 +131,7 @@ var data = {
             "image":""            
         },
         {
-            "factHeading":"THC is toxic to dogs, cats, and most other companion animals!",
+            "factHeading":"THC is toxic to dogs, cats, and most other companion animals.",
             "factBody":"While many humans consume cannabis for the psychoactive effects of THC, it is highly toxic to most other animals. Care should be taken to ensure pets and other animals cannot access cannabis.",
             "type":"Dog",
             "link":"https://mcgill.ca/oss/article/health/woofer-madness-cannabis-companion-animals-and-what-legalization-means-your-pets",
@@ -130,7 +145,7 @@ var data = {
             "image":""            
         },
         {
-            "factHeading":"Some, but not all fish drink!",
+            "factHeading":"Some, but not all fish drink.",
             "factBody":"Saltwater fish absorb oxygen from water through their gills, just like their freshwater kin, but also bring some water into their digestive tract to help their osmoregulation.",
             "type":"Fish",
             "link":"https://mcgill.ca/oss/article/you-asked/do-fish-drink",
@@ -138,7 +153,7 @@ var data = {
         },
         {
             "factHeading":"All tortoises are turtles.",
-            "factBody":"But not all turtles are tortoises, rather, tortoises are a special group of terrestrial turtles.",
+            "factBody":"But not all turtles are tortoises, rather, tortoises are a special group of terrestrial turtles, however living on land is not the only distinction. There are also turtles that exclusively live on land that are not tortoises.",
             "type":"Turtle",
             "link":"",
             "image":""            
@@ -202,7 +217,7 @@ var data = {
         {
             "factHeading":"Owls do not have eye 'balls'.",
             "factBody":"Their eyes are actually shaped as cylinders. While this adaptation greatly improves their night-vision, it means they cannot rotate their eyes in their sockets like we can. Instead, owls must move their entire heads to look around, which is why they can turn their heads almost 270 degrees.",
-            "type":"Owl",
+            "type":"Bird",
             "link":"https://mcgill.ca/oss/article/did-you-know/owls-dont-have-eyeballs",
             "image":""            
         },
@@ -246,7 +261,7 @@ var data = {
             "factBody":"Like all other rodents, beaver incisors never stop growing throughout the animal's life. However, because of the intense stress their teeth undergo from cutting down trees, their enamel contains a significant amount of iron to make them stronger and more resistant to acid. The iron gives beaver teeth their distinctive orange colouring, the same reason our blood is red!",
             "type":"Wetland",
             "link":"https://mcgill.ca/oss/article/did-you-know/beavers-have-metal-teeth",
-            "image":""            
+            "image":"Beaver_Teeth.jpg"            
         },
         {
             "factHeading":"Rabbits need to eat their own poop.",
@@ -267,6 +282,55 @@ var data = {
             "factBody":"Pangolins, also called 'Scaly Anteaters', have no teeth. They eat only insects, and so have adapted their mouths to accomodate an very long tongue with sticky saliva to grab bugs out of their nests. To help crush up and digest their food, they also consume many small rocks and pebbles, known as gastroliths. These sit in a special organ called the gizzard, where, along with internal horn-like projections, their food is crushed up and 'chewed' after it is swallowed.",
             "type":"Pangolin",
             "link":"https://mcgill.ca/oss/article/did-you-know-general-science/pangolins-use-rocks-lieu-teeth",
+            "image":""            
+        },
+        {
+            "factHeading":"Bears don't hibernate.",
+            "factBody":"During winter months bears, do remain in their dens and sleep away the cold. However, contrary to popular belief, this is not true hibernation that is observed in other animals, such as squirrels, hedghehods and butterflies. Unlike these animals, a bear's metabolic rate does not lower significantly for their rest to be classified as a form of hibernation.",
+            "type":"Bear",
+            "link":"https://mcgill.ca/oss/article/did-you-know/bears-dont-hibernate",
+            "image":""            
+        },
+        {
+            "factHeading":"Leukemia is highly contagious in cats.",
+            "factBody":"This cancer of the bone marrow is caused by a highly contagious virus, FeLV (Feline Leukemia Virus) in cats, spread by bodily fluids such as saliva, milk, and blood. Fortunately, this virus is not dangerous to humans, so people are at no risk from contracting this disease from their pet. Roughly 70% of cats can fight off the virus fine and not catch leukemia, and there is also a vaccine, although it is not 100% effective and is usually only reccomended for more at-risk cats. Although this disease and its ensuing complications ultimately reduce the lifespan and limit social interactions with others, FeLV positive cats seem to live a normal life with complete quality until the condition worses at the end of its course.",
+            "type":"Cat",
+            "link":"https://mcgill.ca/oss/article/did-you-know/cats-can-get-leukaemia-and-its-contagious",
+            "image":""            
+        },
+        {
+            "factHeading":"Electric eels use specially adapted cells that are similar to muscles to produce shocks.",
+            "factBody":"Many organisms, from bacteria, to plants, to animals, use electricity to perform biologic functions, such as electrical signalling through the nervous system. Electric eels possess specialized cells called electrocytes that seem to resemble muscle cells to electric charges of up to 0.15V. Howeverm this isn't just to protect them from predators, but actually plays an important role in both communication and foraging in murky rivers.",
+            "type":"Fish",
+            "link":"https://mcgill.ca/oss/article/did-you-know/where-do-electric-eels-get-their-electricity",
+            "image":""            
+        },
+        {
+            "factHeading":"Platypuses hunt by tracking their prey's electrical signals.",
+            "factBody":"Platypuses have almost 40'000 specialised cells in their bill that detect the electrical fields generated by the muscles of other animals. The rivers where they live are so murky that hunting by sight is impossible, but following these electrical signals allows them to track their prey. While some other animals, such as eels, fish, sharks, and even bees can also detect electrical signals, platypuses are one of only 3 mammals with this ability. This adaptations is much more helpful in aquatic rather than terresterial environments.",
+            "type":"Monotreme",
+            "link":"https://mcgill.ca/oss/article/did-you-know/platypus-hunt-tracking-their-preys-electrical-outputs",
+            "image":""            
+        },
+        {
+            "factHeading":"Ostritches don't actually stick their head in the sand.",
+            "factBody":"This myth seems to have originated in ancient Rome. Ostritches do not build nests, but bury they eggs in the sand, and so it's possible that the myth originally arose form people observing this behaviour.",
+            "type":"Bird",
+            "link":"https://mcgill.ca/oss/article/did-you-know/ostriches-do-not-really-stick-their-heads-sand",
+            "image":""            
+        },
+        {
+            "factHeading":"Tigers and Jaguars are attracted to the scent of Calvin Kelin's 'Obssesion for Men'.",
+            "factBody":"Researchers at the Bronx Zoo compared the abilities of 23 different perfumes to attract the attention of tigers, and found this scent to be the winner. Field researchers have also begun using perfumes to attract jaguars in Nicargua. A chemical called 'Civetone', made in part from the oil of a small (and adorable!) mammal called the African civet, seems to be responsible for captivating the cats' attention.",
+            "type":"Cat",
+            "link":"https://mcgill.ca/oss/article/did-you-know-general-science/tigers-and-jaguars-are-attracted-scent-calvin-kleins-obsession-men",
+            "image":""            
+        },
+        {
+            "factHeading":"Lemmings are not suicidal.",
+            "factBody":"That lemmings will run together to off a cliff to their collective dooms has been a pervasive myth since this odd behaviour was seemingly featured in a 1958 Disney Nature Documentary, White Wilderness. However, it was later revealed that this was natural that the film crew actually herded and pushed hundreds of lemmings off the edge to their brutal demise for the sake of a more interesting film. Furthermore, a CBC documentary later revealed that lemmings aren't even natively found where White Wilderness was filmed in northern Alberta.",
+            "type":"Arctic",
+            "link":"https://mcgill.ca/oss/article/did-you-know-history/lies-leaping-lemmings",
             "image":""            
         }
     ]    
